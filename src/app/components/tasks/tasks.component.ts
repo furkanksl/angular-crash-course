70116af81fac1b59d381bd17386944ecdbcecc47
furkanksl <furkankoseoglu65@gmail.com>
2021-06-19 22:18:19 +0300

    created db json

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient , HttpHeaders} from '@angular/common/http';
 import { Task } from 'src/app/Task';
 import { TaskService } from '../../services/task.service';
 
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-task';
@@ -8,10 +9,11 @@ import { Task } from '../Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl ='http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   getTasks(): Observable<Task[]>{
-    return of(TASKS);
+    return  this.http.get<Task[]>(this.apiUrl);
   }
 }
