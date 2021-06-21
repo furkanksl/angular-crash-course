7570c7fd1e8e4a6dcaa1eb4da81a72fb680d8206
furkanksl <furkankoseoglu65@gmail.com>
2021-06-21 13:29:13 +0300

    added toggle for reminder

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '../Task';
 
+const httpOptions = {
+  header: new HttpHeaders({
+  'Content-Type' : 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +24,10 @@ export class TaskService {
   deleteTask(task:Task){
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task,);
   }
 }
